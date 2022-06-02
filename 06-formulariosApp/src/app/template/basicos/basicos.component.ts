d7aa70f5411316904b737720ec6f6d54cca54da9
HenrySofka <henry.ferreira@sofka.com.co>
2022-06-02 17:26:26 -0300

    Se termino la sección 16 del Curso de Angular

--- 06-formulariosApp/src/app/template/basicos/basicos.component.ts
+++ 06-formulariosApp/src/app/template/basicos/basicos.component.ts
@@ -11,6 +11,12 @@ export class BasicosComponent implements OnInit {
 
   @ViewChild('miFormulario') miFormulario!: NgForm;
 
+  initForm = {
+    producto: 'Konosuba: 2',
+    precio: 0,
+    existencias:0
+  }
+
 
   constructor() { }
 
@@ -19,18 +25,23 @@ export class BasicosComponent implements OnInit {
 
 
 
-  nombreValido(): boolean{
-    return this.miFormulario?.controls["producto"]?.invalid && 
-           this.miFormulario?.controls["producto"]?.touched
+  nombreValido(): boolean {
+    return this.miFormulario?.controls["producto"]?.invalid &&
+      this.miFormulario?.controls["producto"]?.touched
   }
 
-  precioValido(): boolean{
+  precioValido(): boolean {
     return this.miFormulario?.controls["precio"]?.value < 0 &&
-           this.miFormulario?.controls["precio"]?.touched
+      this.miFormulario?.controls["precio"]?.touched
   }
 
-  guardar(){
-    console.log(this.miFormulario.value)
+  guardar() {
+    // console.log(this.miFormulario.value)
+    console.log('Posteo correcto');
+    this.miFormulario.resetForm({
+      precio: 0,
+      existencias: 0
+    });
   }
 
 }
--- 06-formulariosApp/src/app/template/dinamicos/dinamicos.component.ts
+++ 06-formulariosApp/src/app/template/dinamicos/dinamicos.component.ts
@@ -1,4 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+// Interfaces Locales
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
+
+
 
 @Component({
   selector: 'app-dinamicos',
@@ -6,11 +20,44 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
 
-  constructor() { }
+  persona:Persona = {
+    nombre: 'Henry',
+    favoritos:[
+      { id: 1, nombre: 'Dark Souls: Prepare to die Edition' },
+      { id: 2, nombre: 'Konosuba: Fantastic Days' },
+    ]
+  }
+  nuevoJuego:string = '';
 
-  ngOnInit(): void {
+
+
+
+  @ViewChild('formDinamico') formDinamico!: NgForm;
+
+  // Método para validar el string en el campo nombre
+  nombreValido(): boolean {
+    return this.formDinamico?.controls["nombre"]?.invalid &&
+           this.formDinamico?.controls["nombre"]?.touched
+  }
+
+  guardar(){
+    console.log('Formulario posteado');
+  }
+
+  eliminar(index:number){
+    this.persona.favoritos.splice(index, 1);
+  }
+
+  agregarJuego(){
+    const nuevoFavorito: Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego
+    }
+
+    this.persona.favoritos.push({...nuevoFavorito});
+    this.nuevoJuego = '';
   }
 
 }
